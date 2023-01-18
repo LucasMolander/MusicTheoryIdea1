5f8bf62b602572880b9e5c2002e205aaec084f81
Lucas Molander <lucasmolander@live.com>
2023-01-17 20:49:25 -0500

    Suggestions

--- musicLogic.py
+++ musicLogic.py
@@ -1,96 +1,59 @@
+from typing import Dict, List, Optional
 import numpy as np
 import pandas as pd
 import plotly.express as px
 import simpleaudio as aud
 
+# TODO Use a map like this rather than a bunch of if/else statements
+NOTE_TO_FREQ: Dict[str, float] = {
+    "A": 220.0,
+    "A#": 233.08,
+    "Bb": 233.08,
+    # etc...
+}
+
+# TODO Use a map like this rather than some if/else statements
+QUAL_TO_MULTS: Dict[str, List[float]] = {
+    "Major7": [1.0, 5/4, 3/2, 15/8, 5/2, 3.0, 15/4, 5.0],
+    # etc...
+}
+
 #finds f1 fundamental frequency for any musical note letter name
-def noteToFreq(noteName):
-    if (noteName == "A"):
-        f1 = 220.0
-    elif (noteName == "A#" or noteName == "Bb"):
-        f1 = 233.08
-    elif (noteName == "B"):
-        f1 = 246.94
-    elif (noteName == "C"):
-        f1 = 261.63
-    elif (noteName == "C#" or noteName == "Db"):
-        f1 = 277.18
-    elif (noteName == "D"):
-        f1 = 293.66
-    elif (noteName == "D#" or noteName == "Eb"):
-        f1 = 311.13
-    elif (noteName == "E"):
-        f1 = 329.63
-    elif (noteName == "F"):
-        f1 = 349.23
-    elif (noteName == "F#" or noteName == "Gb"):
-        f1 = 369.99
-    elif (noteName == "G"):
-        f1 = 196.0
-    elif (noteName == "G#" or noteName == "Ab"):
-        f1 = 207.65
-    else:
+# TODO Use the type-hints that Python3 has (List, Optional, Dict, etc.)
+def getFreqForNote(noteName) -> Optional[float]:
+    if noteName not in NOTE_TO_FREQ:
         print("Please input a valid note name")
-    
-    return f1
+        return None
+    return NOTE_TO_FREQ[noteName]
+
+def getMultsForQuality(qualName) -> Optional[List[float]]:
+    if qualName not in QUAL_TO_MULTS:
+        print("Please input a valid chord quality(Major, Major7, minor, minor7, or 7)")
+        return None
+    return QUAL_TO_MULTS[qualName]
 
 #calculates f2-8 based on f1 and chord quality
-def chordBuilder8notes(noteName, Quality):
-    f1 = noteToFreq(noteName)
-    if (Quality == "Major7"):
-        f2=f1*5/4
-        f3=f1*3/2
-        f4=f1*15/8
-        f5=f1*5/2
-        f6=f1*3
-        f7=f1*15/4
-        f8=f1*5
-    elif (Quality == "Major"):
-        f2=f1*5/4
-        f3=f1*3/2
-        f4=f1*2
-        f5=f1*5/2
-        f6=f1*3
-        f7=f1*4
-        f8=f1*5
-    elif (Quality == "minor"):
-        f2=f1*6/5
-        f3=f1*3/2
-        f4=f1*2
-        f5=f1*12/5
-        f6=f1*3
-        f7=f1*4
-        f8=f1*24/5
-    elif (Quality == "minor7"):
-        f2=f1*6/5
-        f3=f1*3/2
-        f4=f1*7/4
-        f5=f1*12/5
-        f6=f1*3
-        f7=f1*7/2
-        f8=f1*24/5
-    elif (Quality == "7"):
-        f2=f1*5/4
-        f3=f1*3/2
-        f4=f1*7/4
-        f5=f1*5/2
-        f6=f1*3
-        f7=f1*7/2
-        f8=f1*5
-    else:
-        print("Please input a valid chord quality(Major, Major7, minor, minor7, or 7)")
-    
-    return [f2, f3, f4, f5, f6, f7, f8]
+# TODO Don't use a capital Q in the variable name "Quality"
+def chordBuilder8notes(noteName, qualName) -> Optional[List[float]]:
+    f1 = getFreqForNote(noteName)
+    if f1 is None:
+        return None
 
-#combine and hold chord for T based on 
+    return getMultsForQuality(qualName)
+
+#combine and hold chord for T based on
 # calls: noteToFreq, chodBuilder8notes
-def ChordHold(noteName, Quality, fs, T, wt):
+def ChordHold(noteName, qualName, fs, T, wt):
     #find fundamental frequency based on note name e.g. C or Eb
-    f1 = noteToFreq(noteName)
-    
+    f1 = getFreqForNote(noteName)
+    if f1 is None:
+        return None
+
     #build chord based on frequency ratios associated with chord quality e.g. Major7
-    eightNoteChord = chordBuilder8notes(noteName, Quality)  #returns f2-8
-    
+    eightNoteChord = chordBuilder8notes(noteName, qualName)  #returns f2-8
+    if eightNoteChord is None:
+        return None
+
     #start with sine wave of fundatmental
     notes = np.sin(f1 * wt)
 
@@ -120,13 +83,16 @@ def twoEightNotesOffBeat(T,fs):
     return envelope[0:int(T*fs)]
 
 #arpeggiates through all 8 notes in the given numbeer of beats
-def ArpEightSteps(noteName, Quality, fs, T):
-    f1 = noteToFreq(noteName)
-    eightNoteChord = chordBuilder8notes(noteName, Quality)
+def ArpEightSteps(noteName, qualName, fs, T):
+    f1 = getFreqForNote(noteName)
+    if f1 is None:
+        return None
+    eightNoteChord = chordBuilder8notes(noteName, qualName)
+    if eightNoteChord is None:
+        return None
     t=np.arange(0,T/8.0,1.0/fs)                             #samples in time based of period and sampling frequenccy
-    wt = t * 2 * np.pi                                     #multiplier to get freq to w for sin functions  
+    wt = t * 2 * np.pi                                     #multiplier to get freq to w for sin functions
     notes = np.sin(f1*wt)
     for i in eightNoteChord:
         notes = np.hstack((notes, np.sin(i * wt)))
     return notes[0:int(T*fs)]
-
